efd2a7282042ba9eea57f79f5c531990393ea17d
Jens <jnshsrs@users.noreply.github.com>
2025-06-02 22:15:42 +0200

    Update main.tf

--- terraform/main.tf
+++ terraform/main.tf
@@ -34,8 +34,6 @@ resource "azurerm_cognitive_account" "ai_services" {
   sku_name                      = "S0"
   custom_subdomain_name         = "margiesmulti"
   public_network_access_enabled = true
-  restore                       = true 
-
   tags = {
     environment = "demo"
   }
